76ba73dc23cbd645dd08d89eb4156ecdf18150e6
Ihor Gayduk <htgroup.dev@gmail.com>
2013-12-12 14:56:54 +0200

    bug fix

--- start.js
+++ start.js
@@ -5,10 +5,30 @@ var app = express();
 
 var mongoose = require('mongoose');
 
+var user_view = "<h1>Hello, world! This is nCast on Node.js</h1><br><ul>";
 
 mongoose.connect('mongodb://ncast:9322022@ds039487.mongolab.com:39487/ncast');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+
+
+    var users = mongoose.Schema({
+        _id:  String,
+        user: String,
+        pwd:   String,
+        readOnly: Boolean
+    });
+
+    var User = mongoose.model("system.users", users);
+
+
+    User.findOne({}, '', function (err, usr) {
+        user_view += "<li><i>name</i>: "+usr.user+"</li>";
+    });
+
+});
+
 app.configure(function(){
     app.set('port', process.env.PORT || 3000);
     app.set('views', path.join(__dirname, 'views'));
@@ -22,28 +42,7 @@ app.configure(function(){
 });
 
 app.get('/', function(req, res){
-    db.once('open', function callback () {
-
-
-        var users = mongoose.Schema({
-            _id:  String,
-            user: String,
-            pwd:   String,
-            readOnly: Boolean
-        });
-
-        var User = mongoose.model("system.users", users);
-
-        var user_view = "<ul>";
-
-        User.findOne({}, '', function (err, usr) {
-
-            user_view += "<li><i>name</i>: "+usr.user+"</li>";
-
-        });
-
-    });
-    res.end(user_view+"</ul>");
+    res.send(user_view+"</ul>");
 });
 
 if ('development' == app.get('env')) {
