fe61652635ed38ecf99723cbd7916a54e21f47d6
Yaroskav <yarisrespect@gmail.com>
2013-12-12 12:31:55 +0200

    other

--- start.js
+++ start.js
@@ -3,6 +3,13 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://ncast:9322022@ds039487.mongolab.com:39487/ncast');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+    console.log("Connected");
+});
 app.configure(function(){
     app.set('port', process.env.PORT || 3000);
     app.set('views', path.join(__dirname, 'views'));
