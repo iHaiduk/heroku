0e0620b2419ac96b77831e727cb366cd25adb09b
Yaroskav <yarisrespect@gmail.com>
2013-12-13 18:30:17 +0200

    qwe

--- README.md
+++
@@ -1,1 +0,0 @@
-![CodeshipStatus](https://www.codeship.io/projects/d6812b20-4557-0131-916d-361e03ffa97e/status)
--- db/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by HIV on 12.12.13.
- */
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://ncast:9322022@ds039487.mongolab.com:39487/ncast');
-function DB(){
-    return mongoose;
-}
-module.exports = DB();
